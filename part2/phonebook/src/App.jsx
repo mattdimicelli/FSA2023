721de973f37e2998e49984698d78d0313898d19b
MDiMicelli <mdimicelli@northwell.edu>
2023-08-29 15:14:47 -0400

    finish 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas' },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,9 +22,13 @@ const App = () => {
         setNewNumber('');
     }
 
+    const handleChangeFilter = (e) => setFilter(e.currentTarget.value);
+
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+            <h2 style={{display: 'inline-block', marginRight: '1rem', marginBottom: '1rem'}}>Filter by name:</h2>
+            <label>Name<input onChange={handleChangeFilter} /></label>
             <form onSubmit={handleSubmit}>
                 <div>
                     name: <input value={newName} onChange={(e) => setNewName(e.currentTarget.value)} />
@@ -35,12 +43,18 @@ const App = () => {
             <h2>Numbers</h2>
             <table>
                 <tbody>
-                    {persons.map(person => (
-                        <tr key={person.name}>
-                            <td>{person.name}</td><td>{person.number}</td>
-                        </tr>
-                    ))
-                    }
+                    {persons.map(person => {
+                        if (person.name.toLowerCase().includes(filter)) {
+                            return (
+                                <tr key={person.name}>
+                                    <td>{person.name}</td>
+                                    <td>{person.number}</td>
+                                </tr>
+                            )
+                        } else {
+                            return null
+                        }
+                    })}
                 </tbody>
             </table>
         </div>
