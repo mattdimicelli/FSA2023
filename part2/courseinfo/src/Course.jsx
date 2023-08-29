b22858ea1c26bc0e7e2c2f640269c937f7d7f2f5
MDiMicelli <mdimicelli@northwell.edu>
2023-08-29 11:49:06 -0400

    finish 2.4

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,20 +1,22 @@
-const Course = ({course}) => {
-    const {name, parts} = course;
-    return (
-        <figure>
-            <figcaption>{name}</figcaption>
-            <table>
-                <tbody>
-                {parts.map(({exercises, name, id}) => (
-                    <tr key={id}>
-                        <td>{name}</td><td>{exercises}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            <p>total of {parts.reduce((p, c) => c.exercises + p, 0)} exercises</p>
-        </figure>
-    )
+const Course = ({courses}) => {
+    return courses.map(course => {
+        const {name, parts} = course;
+        return (
+            <figure>
+                <figcaption>{name}</figcaption>
+                <table>
+                    <tbody>
+                    {parts.map(({exercises, name, id}) => (
+                        <tr key={id}>
+                            <td>{name}</td><td>{exercises}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <p>total of {parts.reduce((p, c) => c.exercises + p, 0)} exercises</p>
+            </figure>
+        )
+    });
 }
 
 export default Course;
