03145409aeac583ae1a294a0e4d7d114646533f6
Matthew Di Micelli <matthewdimicelli@gmail.com>
2023-09-02 20:26:46 -0400

    finish 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './Filter';
 import PhonebookEntryForm from './PhonebookEntryForm';
 import PhonebookEntries from './PhonebookEntries';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([]);
     const [filter, setFilter] = useState('');
 
+
     const handleChangeFilter = (e) => setFilter(e.currentTarget.value);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => setPersons(res.data));
+    },[]);
 
     return (
         <div>
